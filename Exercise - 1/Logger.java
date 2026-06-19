c4f5d7e55c00c99c401d0f71f1c4d97eab928afe
Parth Tripathi <parth_08@outlook.com>
2026-06-19 12:51:15 +0530

    Final commit message

--- Exercise - 1/Logger.java
+++ Exercise - 1/Logger.java
@@ -1,27 +1,39 @@
 public class Logger {
 
-    // Logger class ka sirf ek object store karne ke liye
+    // Sirf ek object store karne ke liye
     private static Logger instance;
 
-    // Private constructor taki bahar se object na ban sake
+    // Private constructor
     private Logger() {
         System.out.println("Logger instance create ho gaya");
     }
 
-    // Single object return karne wali method
+    // Singleton object return karega
     public static Logger getInstance() {
-
-        // Agar object pehle se nahi bana hai to naya banao
         if (instance == null) {
             instance = new Logger();
         }
-
-        // Wahi same object return karo
         return instance;
     }
 
-    // Log message print karne ki method
+    // Log message print karne ke liye
     public void log(String message) {
         System.out.println("LOG: " + message);
+    }
+
+    // Main method class ke andar hi honi chahiye
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("This is the first log message.");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("This is the second log message.");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
     }
 }
